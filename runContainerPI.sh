e18619b4d4054633c3be03e336374e4aa25cbbee
Florian <floge77>
2020-01-12 16:11:17 +0100

    Add run command for raspberry

---
+++ runContainerPI.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+HOST_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+
+docker run --rm --name cloud2podcast -p 80:80 -v $HOME/downloads:/downloads -e downloadDir=$HOME/downloads -e HOST_IP="cloud2podcast" -it cloud2podcast
