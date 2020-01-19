6b257f4a4cc58e924df67f99b7e0e7ed78ae7317
Oliver Weber <oliver@mondough.io>
2020-01-19 14:13:16 +0100

    test proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function App() {
+  fetch("/podcasts")
+    .then((data) => data.json())
+    .then(console.log);
+
   return (
     <div className="App">
       <header className="App-header">
