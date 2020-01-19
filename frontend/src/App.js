f31061d1b6aa462d042172527b034bdc30a39623
Oliver Weber <oliver@mondough.io>
2020-01-19 14:23:11 +0100

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  fetch("/podcasts")
-    .then((data) => data.json())
-    .then(console.log);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Cloud2Podcasts</h1>
-      </header>
-    </div>
+    <Router>
+      {/* Menu */}
+      <Switch>
+        <Route path="/" children={Home} />
+        <Route path="/config" children={Config} />
+      </Switch>
+    </Router>
   );
 }
+
+// Dummys
+const Home = () => "Home";
+const Config = () => "Config";
 
 export default App;
